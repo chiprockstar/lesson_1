e01e670f9f3fec925523cbc8c9bed4ae3bab5990
Chip Self <chipself9@gmail.com>
2014-11-02 16:00:34 -0500

    fixed indent issue and clarified some variable names

--- paper_rock_scissors.rb
+++ paper_rock_scissors.rb
@@ -18,61 +18,60 @@ end
 def choose
   puts "Choose one: (P/R/S)"
   user_choice = gets.chomp.upcase
-  get_rand_pair(user_choice)
+  get_computer_choice(user_choice)
 end
 
 
-def get_rand_pair(user_choice)
-  key = WINNING_RULES.keys[rand(WINNING_RULES.length)]
-  value = [key, WINNING_RULES[key]]
-  find_winner(value, user_choice)
+def get_computer_choice(user_choice)
+  computer_choice = WINNING_RULES.keys[rand(WINNING_RULES.length)]
+  find_winner(computer_choice, user_choice)
 end
 
 
-def find_winner(value, user_choice)
+def find_winner(computer_choice, user_choice)
   case user_choice
 
   when 'P'
-    if value[0] == :paper
+    if computer_choice == :paper
       puts "It's a tie. \n You picked paper and the computer picked paper."
 
-    elsif value[0] == :rock
+    elsif computer_choice == :rock
       puts "You picked paper and the computer picked rock. \n Paper wraps rock. \n You won!"
 
 
-    elsif value[0] == :scissors
+    elsif computer_choice == :scissors
       puts "You picked paper and the computer picked scissors. \n Scissors cut paper. \n Computer won!"
 
     end
 
-when 'R'
-  if value[0] == :rock
-    puts "It's a tie. \n You picked rock and the computer picked rock."
+  when 'R'
+    if computer_choice == :rock
+      puts "It's a tie. \n You picked rock and the computer picked rock."
 
-  elsif value[0] == :paper
-    puts "You picked rock and the computer picked paper. \n Paper wraps rock. \n Computer won!"
-
-
-  elsif value[0] == :scissors
-    puts "You picked rock and the computer picked scissors. \n Rock smashes scissors \n You won!"
-
-  end
-
-when 'S'
-  if value[0] == :scissors
-    puts "It's a tie. \n You picked scissors and the computer picked scissors."
-
-  elsif value[0] == :paper
-    puts "You picked scissors and the computer picked paper. \n Scissors cut paper. \n You won!"
+    elsif computer_choice == :paper
+      puts "You picked rock and the computer picked paper. \n Paper wraps rock. \n Computer won!"
 
 
-  elsif value[0] == :rock
-    puts "You picked scissors and the computer picked rock. \n Rock smashes scissors. \n Computer won!"
+    elsif computer_choice == :scissors
+      puts "You picked rock and the computer picked scissors. \n Rock smashes scissors \n You won!"
+
+    end
+
+  when 'S'
+    if computer_choice == :scissors
+      puts "It's a tie. \n You picked scissors and the computer picked scissors."
+
+    elsif computer_choice == :paper
+      puts "You picked scissors and the computer picked paper. \n Scissors cut paper. \n You won!"
+
+
+    elsif computer_choice == :rock
+      puts "You picked scissors and the computer picked rock. \n Rock smashes scissors. \n Computer won!"
+
+    end
 
   end
 play_again
-end
-
 end
 
 
