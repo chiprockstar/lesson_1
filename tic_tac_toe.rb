c068cce7b3f294eff552d7b3dae0066d95a44fd7
Chip Self <chipself9@gmail.com>
2014-11-02 21:57:43 -0500

    start of ttt

---
+++ tic_tac_toe.rb
@@ -0,0 +1,22 @@
+
+#
+# def print_grid()
+#   print "\r"
+#   3.times.each  { puts "     |     |"}
+#     puts "-----+-----+-----"
+#     puts "     |     |"
+#     puts "  X  |     |"
+#     puts "     |     |"
+#     puts "-----+-----+-----"
+#   3.times.each  { puts "     |     |"}
+# end
+#
+# print_grid()
+
+#
+# (1..10).each do |percent|
+#   print ""
+#   print ""
+#   print "\rcomplete #{percent*10}%"
+#   sleep(0.5)
+# end
