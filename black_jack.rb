c75e1ac9a4bc00e3ba6a0f7c27a31755176af1ba
Chip Self <chipself9@gmail.com>
2014-11-10 18:16:36 -0500

    first refactor

--- black_jack.rb
+++ black_jack.rb
@@ -1,14 +1,14 @@
 
 
 # black jack game
-# 1. create card deck hash with loops
+# 1. create card deck array with loops
 #     store value, suit, name_of_card
 # 2. deal cards to user and computer
 # 3. store cards each player has into 2 hands
 # 4. remove cards from deck that were delt to 2 hands
 # 5. play: user player receives 2 hits removing cards from deck and adding to the players score
 #  6. player can stop or continue taking hits until his total 21 or more (game over) - store users total score
-#  7. computer continues making plays until he hits at least 17 to 21- store computer total
+#  7. computer continues making plays until it hits at least 17 to 21- store computer total
 #  8. If he goes over 21 game is over.
 
 
@@ -57,171 +57,181 @@ end
 
 
 
-def evaluate_game
-  puts "You win" if players_total == 21
-  puts "Dealer wins" if dealers_total == 21
+def new_game(master_deck, players_total, dealers_total, player_deck, dealer_deck)
+
+  system "clear"
+
+  puts "Dealing..."
+  player_deck.each { |card|  player_say card }
+  player_deck.each { |card| players_total += card[2] }
+  player_deck.each { |card| players_total -= 10 if players_total > 21 && card[1] == 'ace' }
+
+  puts "Score: #{players_total}"
+
+  if players_total == 21
+    puts "You win!"
+    exit
+  end
+
+  dealer_deck.each { |card|  dealer_say card }
+  dealer_deck.each { |card| dealers_total += card[2] }
+  dealer_deck.each { |card| dealers_total -= 10 if dealers_total > 21 && card[1] == 'ace' }
+  puts "Score: #{dealers_total}"
 end
 
-system "clear"
 
+
+
+
+
+
+def players_game(master_deck, players_total, dealers_total, player_deck, dealer_deck)
+
+  begin
+
+  if players_total == 21
+    puts "You win!"
+    exit
+  end
+
+  if dealers_total == 21
+    puts "Dealer wins!"
+    exit
+  end
+
+  if players_total > 21
+    puts "You're Busted, dealer wins!"
+    exit
+  end
+
+  puts "Hit? y / n"
+
+
+  choice = gets.chomp.downcase
+  if choice == 'y'
+    player_deck += deal_cards(master_deck, 1)
+    master_deck = remove_cards(master_deck, 1)
+    system "clear"
+    players_total = 0
+    dealers_total = 0
+    puts "Dealing..."
+    player_deck.each { |card|  player_say card }
+    player_deck.each { |card| players_total += card[2] }
+  end
+
+
+  player_deck.each do |card|
+    if players_total > 21 && card[1] == 'ace'
+     players_total -= 10
+   end
+  end
+
+  puts "Score:  #{players_total}" #if players_total <= 21
+
+  dealer_deck.each { |card|  dealer_say card }
+  dealer_deck.each { |card| dealers_total += card[2] }
+  puts "Score: #{dealers_total}"
+
+
+  end until players_total >= 21 || choice == "n"
+
+result = [master_deck, players_total, dealers_total, player_deck, dealer_deck]
+
+end
+
+
+
+
+
+
+def dealers_game(master_deck, players_total, dealers_total, player_deck, dealer_deck)
+
+begin
 
 players_total = 0
 dealers_total = 0
+
+
+dealer_deck += deal_cards(master_deck, 1)
+master_deck = remove_cards(master_deck, 1)
+
+system "clear"
+
+player_deck.each { |card|  player_say card }
+player_deck.each { |card| players_total += card[2] }
+
+player_deck.each do |card|
+  if players_total > 21 && card[1] == 'ace'
+   players_total -= 10
+ end
+end
+
+
+puts "Score: #{players_total}"
+
+if players_total == 21
+  puts "You win!"
+  exit
+end
+
+if players_total > 21
+  puts "You're busted, dealer wins!"
+  exit
+end
+
+
+puts "Dealing..."
+dealer_deck.each { |card|  dealer_say card }
+dealers_total = 0
+dealer_deck.each { |card| dealers_total += card[2] }
+
+dealer_deck.each do |card|
+  if dealers_total > 21 && card[1] == 'ace'
+   dealers_total -= 10
+ end
+end
+
+puts "Score: #{dealers_total}" #if dealers_total > 21
+puts "Dealer busted, you win!" if dealers_total > 21
+puts "Dealer wins!" if (dealers_total >= players_total) && (dealers_total <= 21) && dealers_total >= 17
+
+end until (dealers_total >= 17 && dealers_total <= 21 && dealers_total > players_total) || (dealers_total > 21)
+true
+end
+
+
+
+
+
+#init game
+
+players_total = 0
+dealers_total = 0
+choice = "y"
+result = [0,0,0,0,0]
+
+system "clear"
+# puts "How many decks would you like to use? 1, 2 or 3"
+# deck_count = gets.chomp.to_i
+#
+# for i in 1..deck_count do
+#    master_deck << master_deck
+# end
+
+puts " Shuffling cards....."
 shuffle_cards(master_deck)
+sleep(2)
+
 player_deck = deal_cards(master_deck, 2)
 master_deck = remove_cards(master_deck, 2)
 dealer_deck = deal_cards(master_deck, 2)
 master_deck = remove_cards(master_deck, 2)
 
-puts "Dealing..."
-player_deck.each { |card|  player_say card }
+new_game(master_deck, players_total, dealers_total, player_deck, dealer_deck)
 
-player_deck.each { |card| players_total += card[2] }
-
-puts players_total
-
-if players_total == 21
-  puts "You win!"
-  exit
-end
-
-dealer_deck.each { |card|  dealer_say card }
-
-dealer_deck.each { |card| dealers_total += card[2] }
-
-puts dealers_total
-
-#p player_deck
-
-begin
-# system "clear"
-if players_total == 21
-  puts "You win!"
-  exit
-end
-
-if dealers_total == 21
-  puts "Dealer wins!"
-  exit
-end
-
-if players_total > 21
-  puts "You're Busted, dealer wins!"
-end
-
-puts "Hit? Y / N"
-choice = gets.chomp.downcase
-if choice == 'y'
-player_deck += deal_cards(master_deck, 1)
-master_deck = remove_cards(master_deck, 1)
-
-#p player_deck
-system "clear"
-
+result = players_game(master_deck, players_total, dealers_total, player_deck, dealer_deck)
 players_total = 0
 dealers_total = 0
-puts "Dealing..."
-player_deck.each { |card|  player_say card }
-player_deck.each { |card| players_total += card[2] }
+if players_total <= 21
+  dealers_game(result[0], players_total, dealers_total, result[3], result[4])
 end
-puts "Score:  #{players_total}" #if players_total <= 21
-
-player_deck.each do |card|
-  if players_total > 21 && card[1] == 'ace'
-   players_total -= 10
-  # puts players_total
- end
-end
-
-dealer_deck.each { |card|  dealer_say card }
-dealer_deck.each { |card| dealers_total += card[2] }
-puts dealers_total
-
-
-end until players_total >= 21 || choice == "n"
-
-
-
-
-
-
-
-
-
-
-
-
-players_total = 0
-dealers_total = 0
-
-begin
-
-if players_total > 21
-  puts "You're busted!"
-  exit
-end
-
-
-if players_total > 21
-  puts "You're busted!"
-  exit
-end
-
-if players_total == 21
-  puts "You win!"
-  exit
-end
-
-if players_total < dealers_total
-  puts "You lose, dealers wins!"
-  exit
-end
-
-players_total = 0
-dealers_total = 0
-system "clear"
-
-choice = 'Y'
-if choice == 'Y'
-    dealer_deck += deal_cards(master_deck, 1)
-    master_deck = remove_cards(master_deck, 1)
-
-
-
-    player_deck.each { |card|  player_say card }
-    player_deck.each { |card| players_total += card[2] }
-    puts players_total
-
-    if players_total == 21
-      puts "You win!"
-      exit
-    end
-
-    if players_total > 21
-      puts "You're busted, dealer wins!"
-      exit
-    end
-
-
-    puts "Dealing..."
-    dealer_deck.each { |card|  dealer_say card }
-    dealers_total = 0
-    dealer_deck.each { |card| dealers_total += card[2] }
-end
-puts dealers_total if dealers_total <= 21
-
-dealer_deck.each do |card|
-  if dealers_total > 21 && card[1] == 'ace'
-   dealers_total -= 10
-   puts dealers_total
- end
-end
-
-
-
-puts dealers_total if dealers_total > 21
-puts "Dealer busted, you win!" if dealers_total > 21
-puts "Dealer wins!" if (dealers_total >= players_total) && (dealers_total <= 21) && dealers_total >= 17
-
-end until (dealers_total >= 17 && dealers_total <= 21 && dealers_total > players_total) || (dealers_total > 21) || (choice == "N")
-#puts dealers_total
